6a62931dfedd58b23fc055b4d97c8ece3ae6e0c1
Igor Mukhin <mukhinid@yandex.ru>
2026-09-16 00:12:34 +0300

    feat: improve main screen

--- app/src/main/java/ru/mukhinid/link2map/MainActivity.kt
+++ app/src/main/java/ru/mukhinid/link2map/MainActivity.kt
@@ -5,10 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,16 +27,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.flow.Flow
@@ -74,24 +89,55 @@ private fun MainScreen(
     }
 
     var showPicker by remember { mutableStateOf(false) }
+    val selectedMapValue = viewModel.selectedMap.collectAsStateWithLifecycle(initialValue = null).value
+    val selectedMap = selectedMapValue?.let {
+        installedMapApps.firstOrNull { it.packageName == selectedMapValue }
+    }
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        OutlinedButton(
+        Column(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxWidth(),
-            onClick = { showPicker = true },
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
         ) {
-            Text(text = "Choose your map app", textAlign = TextAlign.Center)
-        }
-
-        if (showPicker) {
-            AppPickerSheet(
-                installedMapApps,
-                { showPicker = false },
-                { packageName -> viewModel.updateSelectedMap(packageName) },
+            Text(
+                "Selected map app",
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                modifier = Modifier.padding(vertical = 8.dp)
             )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(8.dp))
+                    .clickable(role = Role.Button, onClick = { showPicker = true })
+                    .padding(horizontal = 8.dp, vertical = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                if (selectedMap != null) {
+                    Image(
+                        painter = DrawablePainter(selectedMap.icon),
+                        contentDescription = null,
+                        modifier = Modifier.size(40.dp)
+                    )
+                    Spacer(Modifier.width(16.dp))
+                }
+                Text(
+                    selectedMap?.name ?: "Not specified",
+                    modifier = Modifier.weight(1f),
+                )
+                Icon(Icons.Default.ArrowDropDown, null)
+            }
         }
+    }
+
+    if (showPicker) {
+        AppPickerSheet(
+            installedMapApps,
+            { showPicker = false },
+            { packageName -> viewModel.updateSelectedMap(packageName) },
+        )
     }
 }
 
